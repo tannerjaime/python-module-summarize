caeeb2b178f1eaeee0f324db086a64db4d82d5a4
Aaron Hill <aaron.hill@me.com>
2016-06-03 16:42:36 -0400

    first commit

---
+++ exploration.py
@@ -0,0 +1,18 @@
+""" A module to summarize and visualize a numpy ndarray or a pandas dataframe or series """
+
+def check_input_data(): #ANDREW
+	""" check that input data is a pandas dataframe or series or a numpy ndarray """
+	""" convert input to pandas dataframe (if not one already) """
+	""" return error if input is not any of these data structures """
+
+def check_data_type(): #ANDREW
+	""" simple function to check data type """
+
+class Explore():
+	""" summarize and visualize data """
+
+	def __init__(self, data):
+		self.data = data
+
+	def summarize():
+		""" method to summarize and visualize self.data """
